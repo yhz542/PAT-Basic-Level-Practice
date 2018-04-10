73a0aa8537eacae2f40a394c8343ee108c9bedbf
yobo <33770985+yhz542@users.noreply.github.com>
2018-04-10 18:10:35 +0800

    Update 1033. 旧键盘打字.py

--- 1033. 旧键盘打字.py
+++ 1033. 旧键盘打字.py
@@ -1,7 +1,7 @@
 import sys
 brokenkeys = set( sys.stdin.readline().strip() )
 words = sys.stdin.readline().strip()
-shift = 0 if '+' in brokenkeys else 1
+shift = 0 if '+' in brokenkeys else 1 #记录上档键是否损坏 损坏记 0 否则记 1
 for i in words :
     if i.upper() not in brokenkeys :
         if i.isupper():
