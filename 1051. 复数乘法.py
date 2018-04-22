11632363ec4eab62536cfa54e8e3d99a193d8be5
yobo <33770985+yhz542@users.noreply.github.com>
2018-04-22 16:26:06 +0800

    Create 1051. 复数乘法.py

---
+++ 1051. 复数乘法.py
@@ -0,0 +1,8 @@
+from math import *
+R1 , P1 , R2 , P2 = map( float , input().split() )
+A = R1 * R2 * cos( P1 ) * cos( P2 ) - R1 * R2 * sin( P1 ) * sin( P2 )
+B = R1 * R2 * cos( P1 ) * sin( P2 ) + R1 * R2 * sin( P1 ) * cos( P2 )
+A = 0 if -0.004 <= A <= 0.004 else A
+B = 0 if -0.004 <= B <= 0.004 else B
+print( '%.2f'%A , end = '' )
+print( '{}{:.2f}i'.format( '+' if B >= 0 else '' , B ) ) 
