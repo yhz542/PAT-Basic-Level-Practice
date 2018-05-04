68edef11f36c657c65bdf1946dff85e7e9964cfd
yobo <33770985+yhz542@users.noreply.github.com>
2018-05-04 21:17:40 +0800

    Update 1060. 爱丁顿数.py

--- 1060. 爱丁顿数.py
+++ 1060. 爱丁顿数.py
@@ -2,15 +2,18 @@ import sys
 N = int( sys.stdin.readline() )
 Data = list( map( int , sys.stdin.readline().split() ) )
 Data.sort( reverse = True)
-answer = 0
+flag = 0
 for index, i in enumerate( Data ):
     if i <= (index+1) :
+        flag = 1
         break
-    answer += 1
-print( answer )
+print( index if flag else index + 1 )
 '''思路：没什么好说的 逆序排序后，进行遍历，当当前元素值小于等于下标时跳出循环，（即公里数小于等于天数）
-再给一组测试数据能跑过就是对的
+额外两组测试数据
 4
 5 4 4 1
 答案是3
+3
+4 4 4
+答案还是3
 '''
