d45d582770b7c8ad7549ffb5d25b5bd3a84aed53
yobo <33770985+yhz542@users.noreply.github.com>
2018-10-04 12:36:00 +0800

    Update 1074. 宇宙无敌加法器.py

--- 1074. 宇宙无敌加法器.py
+++ 1074. 宇宙无敌加法器.py
@@ -1,19 +1,19 @@
 import sys
 N = sys.stdin.readline().rstrip()
-PAT1 = sys.stdin.readline().rstrip()
+PAT1 = sys.stdin.readline().rstrip()#将输入数字当作字符串处理 会比较方便也可以读入整数后对10整除 读入数组处理
 PAT2 = sys.stdin.readline().rstrip()
 len1 = len( PAT1 )
 len2 = len( PAT2 )
-difference = len1 - len2
+difference = len1 - len2#比较两个输入的长度 短的一方补0，方便统一计算
 if difference > 0 :
     PAT2 = ''.join(['0'*difference , PAT2 ])
     length = len1
 elif difference < 0 :
     PAT1 = '0'*(-difference) + PAT1
     length = len2
-answer = [ 0 for i in range(len(N)+1)]
-begin = -1
-forward = 0
+answer = [ 0 for i in range(len(N)+1)]#创造答案的格式，注意由于有进位的存在 所以位数最多比进制表 N的进制还要多1位
+begin = -1             #从末位开始计算
+forward = 0            #记录是否进位 0不进 1 进
 for i in N[::-1]:
     num = int( i )
     number = int( PAT1[begin] ) + int( PAT2[begin] )+forward
@@ -25,7 +25,7 @@ for i in N[::-1]:
         forward = 1
     answer[begin] = number
     begin-=1
-if forward == 1:
+if forward == 1:#如果最后进位为 1 在头位补一个1
     answer[begin] = 1
 for i,value in enumerate(answer):
     if value:
