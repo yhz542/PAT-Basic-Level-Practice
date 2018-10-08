3258129dd7c055e040174001ee4b856c95038836
yobo <33770985+yhz542@users.noreply.github.com>
2018-10-08 10:53:48 +0800

    Update 1078. 字符串压缩与解压.py

--- 1078. 字符串压缩与解压.py
+++ 1078. 字符串压缩与解压.py
@@ -1,5 +1,5 @@
 import sys#代码未优化过，应该可以写得更简单一点。 题目需要注意的是解压的时候 数字不一定是1位 也可能是2位 3位 4位 处理时需要把之间的数字保存下来
-
+#https://blog.csdn.net/qq_37430374/article/details/78816224 别人写的C++版本 稍微改改就可以变成C语言版本
 def zipped( string ):
     former = string[ 0 ]
     cnt = 1
