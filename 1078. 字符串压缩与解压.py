0d6e23fdca38f4af8a2aea707a110eac5779a6be
yobo <33770985+yhz542@users.noreply.github.com>
2018-10-08 10:52:18 +0800

    Create 1078. 字符串压缩与解压.py

---
+++ 1078. 字符串压缩与解压.py
@@ -0,0 +1,51 @@
+import sys#代码未优化过，应该可以写得更简单一点。 题目需要注意的是解压的时候 数字不一定是1位 也可能是2位 3位 4位 处理时需要把之间的数字保存下来
+
+def zipped( string ):
+    former = string[ 0 ]
+    cnt = 1
+    for value in string[ 1 : ] :
+        if value == former :
+            cnt += 1
+        else:
+            if cnt > 1:
+                addition = str( cnt ) + former
+            else:
+                addition = former
+            print( addition , end = '' )
+            cnt = 1
+            former = value
+    else:
+        if cnt > 1:
+            addition = str( cnt ) + former
+        else:
+            addition = former
+        print( addition )
+
+def unzipped( string ):
+    former = string[ 0 ]
+    if former.isdigit():
+        cnt = former
+    else:
+        print( former , end ='')
+        cnt ='0'
+    for i in string[1: ]:
+        if i.isdigit() :
+            if former.isdigit():
+                cnt = cnt + i
+            else:
+                cnt = i
+        else:
+            cnt = int( cnt )
+            if cnt :
+                print( cnt * i ,end = '')
+            else:
+                print( i , end ='' )
+            cnt ='0'
+        former = i
+
+NeedZip = sys.stdin.readline().rstrip()
+string = sys.stdin.readline().rstrip('\n')
+if NeedZip == 'C':
+    answer = zipped( string )
+else:
+    answer = unzipped( string )
